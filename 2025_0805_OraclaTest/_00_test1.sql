b92e94e445af863d12457cf8b97608f84ec8c43b
HappyLeo7 <suh0007@naver.com>
2025-08-06 18:09:58 +0900

    test 테이블 만들기

--- 2025_0805_OraclaTest/_00_test1.sql
+++ 2025_0805_OraclaTest/_00_test1.sql
@@ -3,4 +3,20 @@ create
 create
 create
 create table 자바공부(no int ,name int
-			ㅁ		
+			
+
+--테이블 생성
+create table 대학교
+	(학번 int,
+	학년 int,
+	전공 varchar2(100),
+	전화번호 varchar2(100)
+	)
+
+--구조변경 제약걸기
+alter table 대학교
+	add constraint unique_대학교_학번 unique(학번)
+	
+--제약 체크하기
+select * from user_constraints where table_name='대학교'
+	
